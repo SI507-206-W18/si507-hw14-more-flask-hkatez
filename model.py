7bde0b236aac5bc68e7775d4dd903221f3f78f90
huanzhao <you@example.com>
2018-04-22 04:50:51 -0400

    add modified homework

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+next_id = 0
 
 def init(app):
     global entries
@@ -21,11 +22,23 @@ def get_entries():
     return entries
 
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+    global entries, GUESTBOOK_ENTRIES_FILE, next_id
     now = datetime.now()
     time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    entry = {"author": name, "text": text, "timestamp": time_string, "id": str(next_id)}
+    next_id += 1
     entries.insert(0, entry) ## add to front of list
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        dump_string = json.dumps(entries)
+        f.write(dump_string)
+        f.close()
+    except:
+        print("ERROR! Could not write entries to file.")
+
+def delete_entry(id):
+    global entries, GUESTBOOK_ENTRIES_FILE
+    entries = [i for i in entries if i['next_id'] != id]
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
         dump_string = json.dumps(entries)
